8be1945e148669eee341cade702dfe1481ed3324
simera <olman4jesus@gmail.com>
2025-02-07 14:35:16 +0300

    refactor: update Supabase key handling and enhance cabin management features

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,12 +1,21 @@
-import supabase from "./supabase";
+import supabase from './supabase'
 
 export async function getCabins() {
-  const { data, error } = await supabase.from("cabins").select("*");
+	let { data: cabins, error } = await supabase.from('cabins').select('*')
 
-  if (error) {
-    console.error(error);
-    throw new Error("Cabins could not be loaded");
-  }
+	if (error) {
+		console.error(error)
+		throw new Error('Cabins could not be loaded')
+	}
 
-  return data;
+	return cabins
+}
+
+export async function deleteCabin(id) {
+	const { error } = await supabase.from('cabins').delete().eq('id', id)
+
+	if (error) {
+		console.error(error)
+		throw new Error('Cabins could not be deleted')
+	}
 }
