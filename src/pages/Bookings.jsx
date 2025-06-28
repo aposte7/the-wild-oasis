f7711c19b83a8ab6f2af2187c7ecce14f07eac0b
aposte7 <olman4jesus@gmail.com>
2025-06-29 00:59:17 +0300

    refactor: streamline BookingTable and enhance Booking page layout

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,7 +1,21 @@
 import BookingTable from '../features/bookings/BookingTable'
+import BookingTableOperations from '../features/bookings/BookingTableOperations'
+import Heading from '../ui/Heading'
+import Row from '../ui/Row'
 
 function Booking() {
-	return <BookingTable />
+	return (
+		<>
+			<Row type="horizontal">
+				<Heading as="h1">All Bookings</Heading>
+				<BookingTableOperations />
+			</Row>
+
+			<Row>
+				<BookingTable />
+			</Row>
+		</>
+	)
 }
 
 export default Booking
