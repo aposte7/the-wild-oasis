c1f41419b91bad14f93a18e1afb6829105630241
aposte7 <olman4jesus@gmail.com>
2025-06-29 01:16:51 +0300

    feat: add BookingDetail route for individual booking views

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Login from './pages/Login'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from './ui/AppLayout'
 import { Toaster } from 'react-hot-toast'
+import BookingDetail from './features/bookings/BookingDetail'
 
 function App() {
 	const queryClient = new QueryClient({
@@ -37,6 +38,10 @@ function App() {
 						/>
 						<Route path="dashboard" element={<Dashboard />} />
 						<Route path="bookings" element={<Bookings />} />
+						<Route
+							path="bookings/:bookingId"
+							element={<BookingDetail />}
+						/>
 						<Route path="cabins" element={<Cabins />} />
 						<Route path="users" element={<Users />} />
 						<Route path="settings" element={<Settings />} />
